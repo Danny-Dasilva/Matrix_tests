08194cb008be958133f113470155b6bb6320ea74
Danny-Dasilva <yahchayildasilva@gmail.com>
2020-12-29 19:00:58 -0500

    workon perspective matrix

---
+++ angle/test.py
@@ -0,0 +1,12 @@
+# 3d_points = []
+# 2d_features = []
+# for i = 1:4   
+   # found, 2d_features = findPattern(size_of_Pattern_i)
+   # if found:
+      # 3d_points.append(3d_positions_of_pattern_i)
+      # 2d_points.append(2d_features)
+
+# if len(3d_points)> 0:
+  # print "at least one marker visible"
+  # camPose = solvePnP(3d_features, 2d_features, IntrinsicCalibrationData)
+
